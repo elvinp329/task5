5aff0fa56f757ab61dde11b65321935315408d0e
Elvinp329 <elvinkj@code.edu.az>
2023-01-02 22:54:06 +0400

    taskfive

---
+++ taskfive/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace taskfive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int a = int. Parse(Console.ReadLine());
+            int b = int. Parse(Console.ReadLine());
+
+            int kvadrati;
+            for ( int i = (int)Math.Sqrt(a); i <= (int)Math.Sqrt(b); i++ )
+            { 
+                kvadrati = i * i;
+                if (kvadrati  >= a)
+                    Console.WriteLine(kvadrati );
+
+            }
+    
+            Console.WriteLine("Count:" ); 
+        }
+    }
+}
